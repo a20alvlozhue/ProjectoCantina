c2f484213283a3005ccbb01d45de22cca140549b
a17oril <a17orilopmon@inspedralbes.cat>
2021-10-20 10:18:08 +0200

    Cookies
    Cookies

--- comandaf.php
+++ comandaf.php
@@ -9,6 +9,26 @@
 </head>
 <body>
 <h1>Finalització de comanda</h1>
+
+<?php
+echo "<br>";
+if( isset( $_COOKIE["Limitador"]) )
+{
+    echo "La cookie ya existe";
+    echo "El valor de la cookie Limitador despues de crearla: " . $_COOKIE["Limitador"];
+}
+else {
+    echo "Si no existe, la creamos";
+
+// Se crea cookie con duración de un minuto
+    setcookie("Limitador", 54321, time() + 10, "/");
+
+}
+
+
+
+?>
+
 <form action="comandav.php">
     <input type="submit" name="boton" value="🡨">
 </form>
