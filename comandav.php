0b70b5ab3eca2cb28377936448287d85cdeeff49
a18polpuipui <a18polpuipui@inspedralbes.cat>
2021-10-25 12:18:18 +0200

    new

--- comandav.php
+++ comandav.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/normalize.css">
+   
     <title>Validació comanda</title>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
@@ -24,7 +23,7 @@
 
 <div class="dades_comanda">
 
-    <div class="tit">
+    <div class="dadesV">
 
        <h3>Dades comanda</h3>
        
