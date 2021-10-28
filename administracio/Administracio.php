a0cff6dda9815de7f016cd3fb8a469db66ee1b5a
a18polpuipui <a18polpuipui@inspedralbes.cat>
2021-10-28 09:51:15 +0200

    a

---
+++ administracio/Administracio.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<?php
+    require_once('header.php');
+    ?>
+
+
+    <title>Inici</title>
+</head>
+<body>
+<h1>Pantalla administració</h1>
+
+</head>
+
+<body>
+<SCRIPT  language=JavaScript> 
+function go(){
+
+if (document.form.password.value=='admin' && document.form.login.value=='admin'){ 
+        document.form.submit(); 
+    } 
+    else{ 
+         alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+    } 
+} 
+</SCRIPT> 
+<FORM name=form action="Administracio2.php">
+
+<P>Usuario:    <INPUT type=text name=login> 
+<P>Contraseña: <INPUT type=password name=password> 
+<INPUT onclick=go() type=button value=Acceder>
+
+</FORM> 
+</body>
+</html>
+
+<form action="inici.php">
+    <br><br>
+    <input type="submit" name="boton" value="Inici">
+</form>
+
+</body>
+</html>
