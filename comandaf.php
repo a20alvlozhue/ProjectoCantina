c32fa0de23f6945fa8abae5e8c9eccb2b9da0f8f
a18polpuipui <a18polpuipui@inspedralbes.cat>
2021-10-29 20:42:17 +0200

    Revert "Merge branch 'main' of https://github.com/a20alvlozhue/ProjectoCantina"
    This reverts commit b63eda6fbd41b4a8878d522e18a50bbf0206bc34, reversing
    changes made to 88e39fc8857b36d40aafa7af7272c7daa68643a7.

--- comandaf.php
+++ comandaf.php
@@ -8,7 +8,7 @@ session_start();
     <?php
     require_once('header.php');
     ?>
-<link rel="stylesheet" href="css/comandaf.css">
+
     <title>comandaf</title>
 </head>
 <body>
@@ -46,41 +46,5 @@ if (isset($_SESSION["preuTotal"]))
 }
 
 ?>
-<div class="Gracies">
-<h1>Comanda correctament Enviada</h1>
-<h3>Moltes Gracies per la seva comanda</h3>
-</div>
-<div class="Tiquethora">
-<?php
-$DateAndTime = date('m-d-Y h:i:s a', time());  
-echo "<b>Dia: $DateAndTime.</b>";
-echo "<hr>"
-
-
-$productesMati = json_decode(file_get_contents('json/menuM.json'),true);
-$productesTarda = json_decode(file_get_contents('json/menuT.json'),true);
-$totsElsProductes = array_merge($productesMati,$productesTarda);
-//  print_r($totsElsProductes);
-$menu = $totsElsProductes;
-
-$total = 0;
-// print_r($_POST);
-foreach ($_POST as $id => $cantidadProducto) {
-    $key = array_search($id, array_column($menu, 'id'));
-    if (intval($cantidadProducto) > 0 && $key > -1) {
-        echo "<p>Nombre de producto: " . $menu[$key]["nom"]. "</p>".
-        "<p>cantidad producto: " . $cantidadProducto . "</p>".
-        "<p>Precio: " . $menu[$key]["preu"]. "</p><hr>";
-
-        $total += $menu[$key]["preu"] * $cantidadProducto; //calcular precio total
-    // $_SESSION["nom"] = $menu[$key]["preu"];
-    }
-}
-$_SESSION["preuTotal"] = $total;
-$data2 = file_get_contents('json/menuT.json');
-"<br><br>";
-echo "<b>Preu Total de los productos: " . $total . "€</b>";
-?>
-</div>
 </body>
 </html>
